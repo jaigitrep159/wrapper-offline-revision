a030b6f209b9fe681fabb829c87c80638555662d
narutofan420 <7981112-narutofan420@users.noreply.gitlab.com>
2021-05-03 19:41:01 -0500

    Remove that

--- _FOR-DEVS/_IN-DEVELOPMENT/darwin/http.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-echo The certificite has errors so you need to trust wrapper offline here https://localhost:4664/
-echo = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-cd $HOME/desktop/wrapper-offline/server
-http-server -p 4664 -S -C the.crt -K the.key
--- _FOR-DEVS/_IN-DEVELOPMENT/darwin/npm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-echo Starting Wrapper Offfline Node.js webserver
-echo = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-cd $HOME/desktop/wrapper-offline/wrapper
-npm start 
