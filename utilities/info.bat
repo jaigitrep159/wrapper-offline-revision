eea031c18f5f1f1fc693d267db1c50e515228488
narutofan420 <73909524+narutofan420@users.noreply.github.com>
2021-05-10 11:08:47 -0500

    It now lists info

--- utilities/info.bat
+++ utilities/info.bat
@@ -1,17 +1,18 @@
 :: Wrapper: Offline (Revision) Info
 :: This file is used across Wrapper: Offline (Revision). It is not organized, but comments for each setting have been added.
 :: You should not be touching this. Offline relies on this file remaining consistent, and it's easy to mess that up.
+@echo off
 
-:: Opens this file in Notepad when run
-setlocal
-if "%SUBSCRIPT%"=="" ( start notepad.exe config.bat & exit )
-endlocal
+:: List everything
+echo Wrapper version: !WRAPPER_VER!
+echo Wrapper release type: !WRAPPER_TYPE!
+echo Project lead: !PROJECT_LEAD!
 
 :: Wrapper: Offline (Revision) version
 set WRAPPER_VER=1.3.0
 
 :: Wrapper: Offline (Revision) release type
-set WRAPPER_TYPE=public
+set WRAPPER_TYPE=Beta
 
-:: Dark mode in Wrapper. Default: y
-set DARKMODE=y
+:: Wrapper: Offline (Revision) project lead
+set PROJECT_LEAD=narutofan420
